1415dde601f6c93d5a07286f6e1df85f2c68e002
David Sheffield <davesheff@live.co.uk>
2018-02-18 15:08:01 +0000

    humanCatDogYears Test Problem Completed

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 import { humanCatDogYears } from '../src'
-
+describe('humanCatDogYears', () => {
+    it('returns array of human, cat and dog years when passed human years', () => {
+      expect(humanCatDogYears(0)).toEqual([0, 0, 0])
+      expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+      expect(humanCatDogYears(6.2)).toEqual([6.2, 40.8, 45])
+    })  
+  })
 // Look Ma, no handlebars!!!
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,22 @@
 const humanCatDogYears = (number) => {
-
+//makes human age first element in array
+    let array = [number]
+//condition for if person is 0 years old
+if (number === 0){
+    array[1] = 0
+    array[2] = 0
+    }
+//condition for if person is 1 year old
+else if (number === 1) {
+    array[1] = 15
+    array[2] = 15
+    }
+//condition for all over 2 years
+else {
+    array[1] = (((number -2)*4)+24)
+    array[2] = (((number -2)*5)+24)
+    }
+return array    
 }
 
 export default humanCatDogYears
