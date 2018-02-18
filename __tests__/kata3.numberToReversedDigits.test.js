6b898b0c232b6c7e002caed7c493eed4ea03952e
David Sheffield <davesheff@live.co.uk>
2018-02-18 12:01:07 +0000

    numberToReversedDigits Test Problem Completed

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,8 @@ import { numberToReversedDigits } from '../src'
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    
+    expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1])
+    expect(numberToReversedDigits(246810)).toEqual([0, 1, 8, 6, 4 ,2])
+    expect(numberToReversedDigits(1010101010)).toEqual([0, 1, 0, 1, 0, 1, 0, 1, 0, 1])  
   })
 })
--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,8 @@
 const numberToReversedDigits = (number) => {
-
+let array =  (""+number).split("")
+let array2 = array.map(Number)
+array2.reverse()
+return array2
 }
 
 export default numberToReversedDigits
