3f02b14e31123a60ea545f89e5c4fa6db7290286
David Sheffield <davesheff@live.co.uk>
2018-02-18 09:13:16 +0000

    FizzBuzz test problem completed

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,16 @@
 const fizzBuzz = (number) => {
-
+    const isDivisableBy3 = (number % 3) === 0
+    const isDivisableBy5 = (number % 5) === 0
+    if (isDivisableBy5 && isDivisableBy3 ){
+    return 'FizzBuzz'
+    }
+    if (isDivisableBy5) {
+    return 'Buzz'
+    }
+    if (isDivisableBy3) {
+    return 'Fizz'
+    }
+    return number
 }
 
 export default fizzBuzz
