c2a52d35510920aa9393e2ee31f358424e6cd156
SharifCoding <macbook@Apples-MacBook-Pro.local>
2018-02-19 16:35:31 +0000

    updated kata4 file

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,10 +1,36 @@
 const humanCatDogYears = (number) => {
     
-    const CatYears = (24) + (number - 2) * 4
-    const DogYears = (24) + (number - 2) * 5
+    let array = [number]
+    // new array with human year as first element - array [0]
+    // cat year as second element - array [1]
+    // dog year as third element - array [2]
 
-    return number + ', ' + (CatYears) + ', ' + (DogYears)
-    // returns array of human, cat and dog years when passed human years    
+    if (number === 0) {
+        array[1] = 0
+        array[2] = 0
+    }
+    // if human year is 0 years old.
+
+    else if (number === 1) {
+        array[1] = 15
+        array[2] = 15
+    }
+    // if human year is 1 years old.
+
+    else if (number === 2) {
+        array[1] = 24
+        array[2] = 24
+    }
+    // if human year is 2 years old.
+
+    else {
+        array[1] = (24) + (number - 2) * 4
+        array[2] = (24) + (number - 2) * 5
+    }
+    // if human year is 3 years old or over.
+
+    return array
+    // returns array of human, cat, and dog years when passed human years.    
 }
 
 export default humanCatDogYears
