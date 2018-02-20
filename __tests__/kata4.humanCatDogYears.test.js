55ef7aac991508db70f730b20d1f1244e17647cf
Joe Allen <archerpark@hotmail.com>
2018-02-20 15:01:18 +0000

    done

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 import { humanCatDogYears } from '../src'
 
-// Look Ma, no handlebars!!!
+it('returns object of human cat and dog year equivalents when passed age ', () => {
+    
+
+    expect(humanCatDogYears(10)).toEqual({human: 10, cat: 60, dog: 70})
+
+  })
