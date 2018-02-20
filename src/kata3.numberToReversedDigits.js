afc65afba75886427ca40882ba4970fc55b913f2
Joe Allen <archerpark@hotmail.com>
2018-02-20 14:54:17 +0000

    numberToReversed solved

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,8 @@
 const numberToReversedDigits = (number) => {
-
+let numberString = number.toString();
+let stringArray = numberString.split("");
+let arrayReverse = stringArray.reverse();
+return arrayReverse;
 }
 
 export default numberToReversedDigits
