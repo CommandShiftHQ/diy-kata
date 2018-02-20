da76e048fce9fe49b1266c49e8547b642247e88c
Joe Allen <archerpark@hotmail.com>
2018-02-20 14:51:08 +0000

    boolean solved

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,13 @@
 const booleanToWord = (boolean) => {
 
-}
+    if(boolean===true){ 
+    return 'true';
+    }
+    if (boolean===false){
+    return 'false';
+    }
+    return 'not a boolean'
+};
+
 
 export default booleanToWord
