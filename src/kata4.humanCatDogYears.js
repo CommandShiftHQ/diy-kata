76ebc55ab7fbe4d49e1236b8829f75b9b8a59ee6
Joe Allen <archerpark@hotmail.com>
2018-02-20 14:55:21 +0000

    humanCatDogYears solved

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,13 @@
 const humanCatDogYears = (number) => {
-
-}
+    let humanYears = number;
+    let catYears = number * 6;
+    let dogYears = number * 7;
+    let years = {
+        human: humanYears,
+        cat: catYears,
+        dog: dogYears
+    };
+    return years;
+};
 
 export default humanCatDogYears
