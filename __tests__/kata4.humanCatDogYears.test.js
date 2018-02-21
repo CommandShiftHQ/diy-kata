6481d19a90660f07a1b38a028ef6b612c300401e
Clover Chapman <clo@cloverseeds.co.uk>
2018-02-21 10:10:55 +0000

    passing humanCatDogYears test

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
 import { humanCatDogYears } from '../src'
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+    it('returns array of human, cat and dog years when passed human years', () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+        expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    })
+})
