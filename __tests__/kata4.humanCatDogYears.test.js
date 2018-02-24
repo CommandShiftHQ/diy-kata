55c9ecdc159e6a9874c160a53f0c0696180cfb06
Reetu Child <rdhir79@hotmail.co.uk>
2018-02-24 21:14:56 +0000

    added kata4 test

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 import { humanCatDogYears } from '../src'
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+    it('returns an array of human cat and dog years when human years is passed', () => {
+      expect(humanCatDogYears(10)).toEqual([10, 56, 64])  
+    })
+  })
+ 
