c493f1db52534773b65ed305e85a0427617b5a40
Stephen Royle <stephenjroyle@gmail.com>
2018-06-05 21:17:51 +0100

    booleanToWord test completed

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,9 @@
 const booleanToWord = (boolean) => {
-
+    if (boolean === false || boolean === 0 || boolean === "" || boolean === null || boolean === undefined || boolean === NaN) {
+        return 'No'
+    } else {
+        return 'Yes'
+    }
 }
 
 module.exports = booleanToWord;
