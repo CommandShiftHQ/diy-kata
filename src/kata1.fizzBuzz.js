1755e7932a1d95284f480abe139bd5cd6ba2b92e
Daniel-Cross <crossy1686@gmail.com>
2018-06-05 21:24:52 +0100

    Completed fizzBuzz challenge

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,14 @@
 const fizzBuzz = (number) => {
-
-}
+    if (number % 5 === 0 && number % 3 === 0) {
+        return 'FizzBuzz'
+    } 
+    if (number % 5 === 0) {
+            return 'Buzz'
+        } if (number % 3 === 0) {
+            return 'Fizz'
+        } else {
+            return number
+        }
+};
 
 module.exports = fizzBuzz;
