63a3fb62867fe6867437ace760c078d6047f6c2d
Stephen Royle <stephenjroyle@gmail.com>
2018-06-05 20:53:39 +0100

    fizzBuzz test completed

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -2,18 +2,18 @@ const { fizzBuzz } = require('../src');
 
 describe('fizzBuzz', () => {
   it('returns Fizz when passed a multiple of 3', () => {
-
+      expect(fizzBuzz(9)).toEqual('Fizz')
   });
 
   it('returns Buzz when passed a multiple of 5', () => {
-
+      expect(fizzBuzz(10)).toEqual('Buzz')
   });
 
   it('returns FizzBuzz when passed a multiple 3 and 5', () => {
-
+      expect(fizzBuzz(15)).toEqual('FizzBuzz')
   });
 
   it('returns the number when it isn\'t a multiple of 3 or 5', () => {
-
+      expect(fizzBuzz(4)).toEqual(4)
   });
 });
