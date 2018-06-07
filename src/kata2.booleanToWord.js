7ec82664a6854bdae7271190f061d1ea18d820d4
Codenivore <nickolas.mcalpine@gmail.com>
2018-06-07 18:59:00 +0100

    adding comments to numtorevdig file

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,6 @@
 const booleanToWord = (boolean) => {
-    if(boolean === true) {
+    if(boolean === true) { /*explicit check to make sure no none relevant
+                              values submitted*/
         return 'yes'
     } else {
         return 'no'
