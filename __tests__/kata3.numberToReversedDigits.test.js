95c7b973aa05c987a51858ea924cabe2f5c220d1
Carrie Dennett <carriedennett2016@gmail.com>
2018-06-07 20:12:49 +0100

    kata 3 and 4 solved

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,6 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    
+      expect(numberToReversedDigits(12345)).toEqual([5,4,3,2,1])
   });
 });
--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,7 @@
 const { humanCatDogYears } = require('../src');
+    describe('humanCatDogYears', () => {
+      it('provides array of human, cat and dog years for a given number', () => {
+        expect(humanCatDogYears(10)).toEqual([10 , 56 , 64])
+      });
+    });
 
-// Look Ma, no handlebars!!!
