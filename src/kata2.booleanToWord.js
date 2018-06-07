7dcbdab37b518a1ff5d6fe2a6943395d17efe601
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-07 19:03:34 +0100

    update

--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,10 @@
 const booleanToWord = (boolean) => {
-
-}
+    
+    if (boolean) {
+        return 'Yes';
+    } else {
+        return 'No';
+    }
+};
 
 module.exports = booleanToWord;
--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,13 @@
 const numberToReversedDigits = (number) => {
 
+let num = number;
+let output = [];
+
+while (num) {
+    output.push(num % 10);
+    num = Math.floor(num/10);
+}
+return output;
 }
 
 module.exports = numberToReversedDigits;
