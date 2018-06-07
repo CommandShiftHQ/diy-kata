5c3f472ceaf127f0bc6d24fcf799f9def57d31fe
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-07 19:03:34 +0100

    update

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -2,4 +2,13 @@ const { booleanToWord } = require('../src');
 
 describe('booleanToWord', () => {
   // how do we create specs again???
+
+it('returns Yes when booleanToWord is True',() => {
+  expect(booleanToWord(true)).toBe('Yes')
+});
+
+it('returns No when booleanToWord is False', () => {
+  expect(booleanToWord(false)).toBe('No')
+});
+
 });
