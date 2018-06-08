267ade4051f2783b2c6063ff1334eec4e7429b54
Daniel-Cross <crossy1686@gmail.com>
2018-06-09 00:13:11 +0100

    Code for completed test

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,15 @@
 const humanCatDogYears = (number) => {
+    let catYears = number - 2;
+     catYears = catYears * 4;
+     catYears = catYears + 15
+     catYears = catYears + 9;
 
-}
+     let dogYears = number - 2;
+     dogYears = dogYears * 5;
+     dogYears = dogYears + 15;
+     dogYears = dogYears + 9;
+
+     return [number,catYears,dogYears]
+};
 
 module.exports = humanCatDogYears;
