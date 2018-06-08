92eb34918071d4fe57a010899d4cb49091cf53fd
Daniel-Cross <crossy1686@gmail.com>
2018-06-09 00:12:25 +0100

    Completed test code

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,8 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+    it('should return an array of three numbers. The first number is human years, second is the equivalent cat years, third is the equivalent dog years.', () => {
+        expect(humanCatDogYears(31)).toEqual([31,140,169])
+    });
+
+});
