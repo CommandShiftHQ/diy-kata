e3da703c8427e958226316d1e86715f68eb51478
Gaz Ellis <gmiellis@yahoo.co.uk>
2018-06-09 00:02:09 +0100

    updates

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require('../src');
 
 // Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+
+  it('returns array of human, cat and dog years when passed human years', () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+  });
+});
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,26 @@
 const reachDestination = (distance, speed) => {
 
+  function reachDestination (distance, speed) {
+
+    let time = Math.floor(distance) / Math.floor(speed);
+
+    let twoDec = time.toFixed(2);
+
+    let twoDecArr = twoDec.toString().split(".");
+
+        if (twoDecArr[1] >= 0 && twoDecArr[1] <= 49) {
+            twoDecArr[1] = 5;
+        } else {
+            twoDecArr[1] = 0;
+      };
+
+    let result = twoDecArr.join('.');
+
+    return `I should be there in ${result} hours`;
+
+    };
+
+    return reachDestination(distance, speed);
 }
 
 module.exports = reachDestination;
