d30c4d5b3ac4d0ad0284f9c425c99d05c5a7cc71
Daniel-Cross <crossy1686@gmail.com>
2018-06-09 02:08:30 +0100

    Completed test code

--- __tests__/kata5.reachDestination.test.js
+++ __tests__/kata5.reachDestination.test.js
@@ -2,6 +2,6 @@ const { reachDestination } = require('../src');
 
 describe('reachDestination', () => {
   it('returns string with estimated time of arrival', () => {
-    
+    expect(reachDestination(80,120)).toBe('I should be there in about 1 hour')
   });
 });
