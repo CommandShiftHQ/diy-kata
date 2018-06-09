d6aa839627417516385c6ca56d480028760210ef
Daniel-Cross <crossy1686@gmail.com>
2018-06-09 02:09:07 +0100

    Completed code challenge

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,6 @@
 const reachDestination = (distance, speed) => {
-
-}
+    const journeyTime = Math.ceil(distance / speed)
+    return `I should be there in about ${journeyTime} hour`
+};
 
 module.exports = reachDestination;
