36e372fed1ce52ede0e86e5cd19229a3cf48f43d
Daniel-Cross <crossy1686@gmail.com>
2018-06-10 22:29:50 +0100

    Test code completed

--- __tests__/kata7.getEmployerRole.test.js
+++ __tests__/kata7.getEmployerRole.test.js
@@ -2,6 +2,42 @@ const { getEmployerRole } = require('../src');
 
 describe('getEmployerRole', () => {
   it('returns the employee\'s role in the company', () => {
-    
+    expect(getEmployerRole([{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Resources Assistant'
+    }]
+    )).toBe('Developer');
+
+    expect(getEmployerRole([{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Resources Assistant'
+    }]
+    )).toBe('Sales Associate');
+
+    expect(getEmployerRole([{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Resources Assistant'
+    }]
+    )).toBe('Human Resources Assistant');
+
+
   });
 });
