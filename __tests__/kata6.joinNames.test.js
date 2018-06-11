ded0bc05868bd08fb4a487512faf4fc306f23915
Codenivore <nickolas.mcalpine@gmail.com>
2018-06-11 16:52:49 +0100

    updating joinNames files

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -1,7 +1,16 @@
 const { joinNames } = require('../src');
 
 describe('joinNames', () => {
-  it('returns string of names, seperated by commas and an ampersand', () => {
-    
+  it('should return string of names, seperated by commas and an ampersand', () => {
+      expect(joinNames([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}])).toBe('Bart, Lisa & Maggie')
   });
+
+  it('should return string of names, seperated by commas and an ampersand', () => {
+    expect(joinNames([{name: 'John'}, {name: 'Paul'}, {name: 'Ringo'}, {name: 'George'}])).toBe('John, Paul, Ringo & George')
+  });
+
+  it('should return string of names, seperated by commas and an ampersand', () => {
+    expect(joinNames([{name: 'Tom'}, {name: 'Jerry'}])).toBe('Tom & Jerry')
+  });
+
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,11 @@
 const joinNames = (namesObj) => {
 
+ const newArray = namesObj.map(iterator => iterator.name);
+
+    newArray.splice(newArray.length-1, 1, '& '+newArray[newArray.length-1])
+
+    return newArray.join(', ').replace(', &', ' &');
+
 }
 
 module.exports = joinNames;
