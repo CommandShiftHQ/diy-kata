c769230c4303ee8832ae2f92c9a599b187c8d3a1
Daniel-Cross <crossy1686@gmail.com>
2018-06-11 19:16:28 +0100

    Initial push

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,13 @@
 const getEmployerRole = (employeeName, employees) => {
+  const nameArray = employeeName.map(function(x) {
+    return x.role
+ });
 
-}
+
+
+return getEmployerRole('Javid', employees);
+
+};
+
 
 module.exports = getEmployerRole
