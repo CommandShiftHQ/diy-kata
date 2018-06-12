e689e9d0073fb42b044c4edb06c723020d603bce
Stephen Royle <stephenjroyle@gmail.com>
2018-06-12 20:38:55 +0100

    kata6 & kata7 completed

--- __tests__/kata7.getEmployerRole.test.js
+++ __tests__/kata7.getEmployerRole.test.js
@@ -2,6 +2,17 @@ const { getEmployerRole } = require('../src');
 
 describe('getEmployerRole', () => {
   it('returns the employee\'s role in the company', () => {
-    
+    const employees = [{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Resources Assistant'
+    }]
+
+    expect(getEmployerRole('Javid',employees)).toEqual('Human Resources Assistant')
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,10 @@
 const joinNames = (namesObj) => {
+  let names = ''
+  for (let i = 0; i < namesObj.length - 1; i++) {
+    names += namesObj[i].name + ', '
+  }
+    names += namesObj[namesObj.length - 1].name;
 
-}
+  return names.replace(/,(?=[^,]*$)/,' &')}
 
 module.exports = joinNames;
