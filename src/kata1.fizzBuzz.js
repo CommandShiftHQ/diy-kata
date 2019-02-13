c107cd9dc36124c54c806643e4da974b4b61e786
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-13 14:55:08 +0000

    solved fizzBuzz kata1

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,20 @@
 const fizzBuzz = (number) => {
+    const isDivisableBy3 = (number % 3) === 0;
+    const isDivisableBy5 = (number % 5) === 0;
 
-}
+    if (isDivisableBy3 && isDivisableBy5) {
+        return 'FizzBuzz';
+    }
+
+    if (isDivisableBy5) {
+        return 'Buzz';
+    }
+
+    if (isDivisableBy3) {
+        return 'Fizz';
+    }
+  
+    return number;
+};
 
 module.exports = fizzBuzz;
