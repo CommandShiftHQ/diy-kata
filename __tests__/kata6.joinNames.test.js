6a460747abc686c83c745ba71987d0e588108e58
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-14 21:12:38 +0000

    completed joinNames test kata6

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -2,6 +2,21 @@ const { joinNames } = require('../src');
 
 describe('joinNames', () => {
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    const namesObj = [
+      {
+
+        name: 'Bart'
+      
+      }, {
+      
+        name: 'Lisa'
+      
+      }, {
+      
+        name: 'Maggie'
+      
+      }
+    ];
+    expect(joinNames(namesObj)).toEqual('Bart, Lisa & Maggie')
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,20 @@
 const joinNames = (namesObj) => {
+    const extractNames = namesObj.map(item => item.name);
 
+    let stringOfNames = '';
+    if (extractNames.length === 1) {
+        stringOfNames = extractNames[0];
+    }
+
+    if (extractNames.length === 2) {
+        stringOfNames = extractNames.join(' & ');
+    }
+
+    if (extractNames.length > 2) {
+        stringOfNames = extractNames.slice(0, -1).join(', ') + ' & ' + extractNames.slice(-1);
+    }
+
+    return stringOfNames;
 }
 
 module.exports = joinNames;
