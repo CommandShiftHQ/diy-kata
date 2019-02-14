500aeafb6b93addd844dd53b10948c57d9481e02
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-14 00:14:10 +0000

    completed humanCatDogYears test kata4

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears', () => {
+    it('returns and array of numbers of human, cat and dog years', () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15])
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24])
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64])
+    });
+});
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,32 @@
 const humanCatDogYears = (number) => {
+    const catYearOne = 15;
+    const dogYearOne = catYearOne;
+    const catYearTwo = 9;
+    const dogYearTwo = catYearTwo;
+    const catYearThreeOnwards = 4;
+    const dogYearThreeOnwards = 5;
+    const yearsComparison = [];
 
+    if (number < 1) {
+        yearsComparison.push(number, 
+            (number * catYearOne), 
+            (number * dogYearOne));
+        return yearsComparison;
+    }
+
+    if (number <= 2) {
+        yearsComparison.push(number, 
+            (((number - 1) * catYearTwo) + catYearOne), 
+            (((number - 1) * dogYearTwo) + dogYearOne));
+        return yearsComparison;
+    }
+
+    if (number > 2) {
+        yearsComparison.push(number, 
+            (((number - 2) * catYearThreeOnwards) + catYearOne + catYearTwo), 
+            (((number -2) * dogYearThreeOnwards) + dogYearOne + dogYearTwo));
+        return yearsComparison;
+    }
 }
 
 module.exports = humanCatDogYears;
