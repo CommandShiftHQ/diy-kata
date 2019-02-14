a52eedc53d06e5f417c086cbb27bb7bb35c73116
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-14 00:46:52 +0000

    completed reachDestination test kata5

--- __tests__/kata5.reachDestination.test.js
+++ __tests__/kata5.reachDestination.test.js
@@ -2,6 +2,8 @@ const { reachDestination } = require('../src');
 
 describe('reachDestination', () => {
   it('returns string with estimated time of arrival', () => {
-    
+    expect(reachDestination(44, 10)).toBe('I should be there in 4.5 hours.')
+    expect(reachDestination(37, 8)).toBe('I should be there in 5 hours.')
+    expect(reachDestination(107, 14)).toBe('I should be there in 8 hours.')
   });
 });
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,6 @@
 const reachDestination = (distance, speed) => {
-
+    const timeRoundedUp = Math.ceil(distance / speed *2)/2;
+    return `I should be there in ${timeRoundedUp} hours.`;
 }
 
 module.exports = reachDestination;
