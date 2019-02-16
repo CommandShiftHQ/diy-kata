ed793a5f04c095ee589ef375cc0d54c6c95d41d1
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-16 16:11:04 +0000

    completed getEmployerRole test kata7

--- __tests__/kata7.getEmployerRole.test.js
+++ __tests__/kata7.getEmployerRole.test.js
@@ -2,6 +2,26 @@ const { getEmployerRole } = require('../src');
 
 describe('getEmployerRole', () => {
   it('returns the employee\'s role in the company', () => {
+    const employees = [{
+
+      name: 'Satti',
     
+      role: 'Developer'
+    
+    }, {
+    
+      name: 'Jenny',
+    
+      role: 'Sales Associate'
+    
+    }, {
+    
+      name: 'Javid',
+    
+      role: 'Human Recommended Reading Assistant'
+    
+    }];
+    
+    expect(getEmployerRole('Jenny', employees)).toBe('Sales Associate');
   });
 });
--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,9 @@
 const getEmployerRole = (employeeName, employees) => {
-
-}
+    for (let i = 0; i < employees.length; i++) {
+        if (employees[i].name === employeeName) {
+            return employees[i].role;
+        }
+    }
+};
 
 module.exports = getEmployerRole
