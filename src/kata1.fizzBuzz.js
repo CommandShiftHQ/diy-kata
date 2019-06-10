c7489bee4968c5f42c659b9f2ddc07f10d16e53b
Holly <hollyfanthorpe@gmail.com>
2019-06-10 21:27:01 +0100

    Completed FizzBuzz

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,20 @@
 const fizzBuzz = (number) => {
+    const isDivisibleBy3 = ( number % 3) === 0
+    const isDivisibleBy5 = ( number % 5) === 0
 
+    if (isDivisibleBy3 && isDivisibleBy5){
+        return 'FizzBuzz'
+    }
+
+    if (isDivisibleBy5) {
+        return 'Buzz'           
+    }
+
+    if (isDivisibleBy3){
+    return 'Fizz'
+    }
+
+    return number
 }
 
 module.exports = fizzBuzz;
