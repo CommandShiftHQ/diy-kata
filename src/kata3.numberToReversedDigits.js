462ae621c70b38a52c5809d4c918bc858fee91be
Holly <hollyfanthorpe@gmail.com>
2019-06-12 17:55:16 +0100

    Completed number to reversed digits

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,5 +1,7 @@
 const numberToReversedDigits = (number) => {
-
+   const numberArray = Array.from(number.toString()).map(Number)
+   const reversedArray = numberArray.reverse()
+   return reversedArray
 }
 
 module.exports = numberToReversedDigits;
