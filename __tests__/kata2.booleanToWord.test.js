94aeab6b491a7cdd7f504aae7a3a5db08acd3ce0
Holly <hollyfanthorpe@gmail.com>
2019-06-12 17:40:15 +0100

    Completed boolean to words

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -1,5 +1,12 @@
 const { booleanToWord } = require('../src');
 
 describe('booleanToWord', () => {
-  // how do we create specs again???
+  it('returns Yes when passed a True boolean', () =>{
+    expect(booleanToWord(true)).toBe('Yes')
+  
+});
+
+it('returns No when passed a False boolean', () =>{
+  expect(booleanToWord(false)).toBe('No')
+  });
 });
--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,8 @@
 const booleanToWord = (boolean) => {
-
+    if(boolean === true){
+        return 'Yes'
+    }
+    return 'No'
 }
 
 module.exports = booleanToWord;
