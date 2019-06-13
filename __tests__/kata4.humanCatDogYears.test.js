6c0861175bc85c9b3bdaa25c736d5732ba964e9f
Holly <hollyfanthorpe@gmail.com>
2019-06-13 18:54:19 +0100

    Completed humanCatDogYears

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,6 @@
 const { humanCatDogYears } = require('../src');
+describe('humanCatDogYears', () =>{ 
+    it('returns an array of human, cat & dog years')
+    expect(humanCatDogYears(10)).toEqual([10,56,64])
+});
 
-// Look Ma, no handlebars!!!
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,16 @@
 const humanCatDogYears = (number) => {
+    const yearsArray = []
 
-}
+    const catYears = (number) =>{ 
+        return 24 + ((number - 2) * 4)
+    };
+
+    const dogYears = (number) => {
+        return 24 + ((number - 2) * 5 )
+    };
+
+    yearsArray.push(number, catYears(number), dogYears(number))
+    return yearsArray
+};
 
 module.exports = humanCatDogYears;
