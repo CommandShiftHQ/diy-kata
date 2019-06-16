c3cd4add1d2dd5b43094380fa2e9fe57945871fe
Holly <hollyfanthorpe@gmail.com>
2019-06-16 12:48:54 +0100

    Completed join names

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -1,7 +1,35 @@
 const { joinNames } = require('../src');
 
 describe('joinNames', () => {
+const names = [
+  {
+    name: 'Holly'
+  },
+  {
+    name: 'Thomas'
+  },
+  {
+    name: 'Angela'
+  },
+  {
+    name: 'Norman'
+  }
+];
+
+const names2 = [
+  {
+    name: 'Holly'
+  },
+  {
+    name: 'Dee'
+  },
+  {
+    name: 'Trudie'
+  }
+];
+
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    expect(joinNames(names)).toEqual('Holly, Thomas, Angela & Norman')
+    expect(joinNames(names2)).toEqual('Holly, Dee & Trudie')
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,8 @@
-const joinNames = (namesObj) => {
-
+const joinNames = (object) => {
+   let namesArray = object.map(element => element.name)
+    let finalName = namesArray.pop().toString()
+    let stringNames = namesArray.join(', ')
+    return `${stringNames} & ${finalName}`
 }
 
 module.exports = joinNames;
