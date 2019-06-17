3ec9a5a0abeaa972b71a01de528f49bf6f11ae12
Joy Dutta <dutta_joy@hotmail.com>
2019-06-17 20:55:23 +0100

    Completes kata1 Fizzbuzz exercise

--- __tests__/kata1.fizzBuzz.test.js
+++ __tests__/kata1.fizzBuzz.test.js
@@ -1,19 +1,23 @@
-const { fizzBuzz } = require('../src');
+const  fizzBuzz = require('../src/kata1.fizzBuzz');
 
 describe('fizzBuzz', () => {
-  it('returns Fizz when passed a multiple of 3', () => {
-
+  it('returns Fizz when passed a multiple of 3', () => {    
+      expect(fizzBuzz(3)).toBe('Fizz');
+      expect(fizzBuzz(9)).toBe('Fizz');      
   });
 
-  it('returns Buzz when passed a multiple of 5', () => {
-
+  it('returns Buzz when passed a multiple of 5', () => {    
+      expect(fizzBuzz(5)).toBe('Buzz');
+      expect(fizzBuzz(20)).toBe('Buzz');
   });
 
-  it('returns FizzBuzz when passed a multiple 3 and 5', () => {
-
+  it('returns FizzBuzz when passed a multiple 3 and 5', () => {    
+      expect(fizzBuzz(15)).toBe('FizzBuzz');
+      expect(fizzBuzz(75)).toBe('FizzBuzz');
   });
 
-  it('returns the number when it isn\'t a multiple of 3 or 5', () => {
-
+  it('returns the number when it isn\'t a multiple of 3 or 5', () => {    
+      expect(fizzBuzz(7)).toBe(7);
+      expect(fizzBuzz(91)).toBe(91);
   });
 });
--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,24 @@
 const fizzBuzz = (number) => {
+    const isDivisibleBy3 = number % 3 === 0;
+    const isDivisibleBy5 = number % 5 === 0;
+    const isDivisibleBy3And5 = number % 5 === 0  && number % 3 === 0;
 
-}
+ if(isDivisibleBy3And5)
+    {
+        return 'FizzBuzz'
+    }
+else if(isDivisibleBy3)
+    {
+        return 'Fizz'
+    }
+else if(isDivisibleBy5)
+    {
+        return 'Buzz'
+    }
+else
+    {
+        return number
+    }
+};
 
 module.exports = fizzBuzz;
