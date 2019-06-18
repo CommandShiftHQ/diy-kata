61862e7243a78a7d80d62e4f758caa49496af822
Joy Dutta <dutta_joy@hotmail.com>
2019-06-18 11:35:21 +0100

    Completes tdd  kata5 reachDestination exercise

--- __tests__/kata5.reachDestination.test.js
+++ __tests__/kata5.reachDestination.test.js
@@ -1,7 +1,7 @@
-const { reachDestination } = require('../src');
+const reachDestination = require('../src/kata5.reachDestination');
 
 describe('reachDestination', () => {
-  it('returns string with estimated time of arrival', () => {
-    
+    it('returns a string with estimated time of arrival', () => {
+    expect(reachDestination(44, 10)).toEqual('I should be there in 4.5 hours')
   });
 });
--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,7 @@
 const reachDestination = (distance, speed) => {
-
+ const time = distance/speed;
+ const roundTo05Hrs = (time) => Math.round(time * 2) / 2;
+ return `I should be there in ${roundTo05Hrs(time)} hours`
 }
 
 module.exports = reachDestination;
