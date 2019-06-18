7eab72a6e2f3dbfc1ac2a720fdc55ec9e51524c1
Joy Dutta <dutta_joy@hotmail.com>
2019-06-18 12:08:47 +0100

    Completes tdd kata7 getEmployerRole exercise

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,5 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {
-
-}
-
+const getEmployerRole = (employeeName, employees) => { 
+	const employee = employees.find(employee => employee.name === employeeName)
+		return employee.role
+};
+	
 module.exports = getEmployerRole
