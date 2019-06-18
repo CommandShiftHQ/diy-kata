b0ddc0152a9a06f9bc6183c029153bc669ab0c9f
Joy Dutta <dutta_joy@hotmail.com>
2019-06-18 11:55:36 +0100

    Completes tdd kata6joinNames exercise

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -1,7 +1,39 @@
-const { joinNames } = require('../src');
+const joinNames = require('../src/kata6.joinNames');
 
 describe('joinNames', () => {
+  const obj1 = [{
+
+    name: 'Bart'
+  
+  }, {
+  
+    name: 'Lisa'
+  
+  }, {
+  
+    name: 'Maggie'
+  
+  }];
+
+  const obj2 = [{
+
+    name: 'Joy'
+  
+  }, {
+  
+    name: 'Paramita'
+  
+  }, {
+  
+    name: 'Krishna'
+  
+  },{
+  
+    name: 'Luke'
+  
+  }];
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    expect(joinNames(obj1)).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames(obj2)).toEqual('Joy, Paramita, Krishna & Luke')
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,10 @@
 const joinNames = (namesObj) => {
+    
+    const nameArray = namesObj.map(element => element.name);
+	const commaSeperatedNameString = nameArray.join(', ');
+	const lastCommaIndex = commaSeperatedNameString.lastIndexOf(',');
+    return commaSeperatedNameString.substr(0, lastCommaIndex) + ' &' + commaSeperatedNameString.substr(lastCommaIndex + 1);
 
-}
+};
 
 module.exports = joinNames;
