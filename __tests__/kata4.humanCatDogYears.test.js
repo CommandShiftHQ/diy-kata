4a7b9a4c0ded127ce64e51e55e9d98935aadb6c6
Joy Dutta <dutta_joy@hotmail.com>
2019-06-18 10:50:26 +0100

    Completes kata4 humanCatDogYears

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,10 @@
-const { humanCatDogYears } = require('../src');
+const humanCatDogYears = require('../src/kata4.humanCatDogYears');
 
-// Look Ma, no handlebars!!!
+describe('humanCatDogYears',() => {
+    const expected1 = [10, 56, 64];
+    const expected2 = [20, 96, 114];
+ it('returns an array of human, cat and dog years when passed human years', () => {
+    expect(humanCatDogYears(10)).toEqual(expected1);
+    expect(humanCatDogYears(20)).toEqual(expected2);
+ });
+});
--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,5 +1,7 @@
-const humanCatDogYears = (number) => {
-
-}
+const humanCatDogYears = number => {
+    const human2CatYrs = 15 + 9 + 4*(number -2);
+    const human2DogYrs = 15 + 9 + 5* (number - 2);
+    return [number, human2CatYrs,human2DogYrs]
+   };   
 
 module.exports = humanCatDogYears;
