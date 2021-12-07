6568d8569a7262d56745de8f2c93f63f954bc252
Wxmnath <Wxmnath@aol.com>
2021-12-07 21:14:30 +0000

    numberToReverseDigits Completed

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,12 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = (number) =>
+  number.map(
+    number
+      .toString()
+      .split("")
+      .reverse()
+      .join("")
+  );
+{
+}
 
 module.exports = numberToReversedDigits;
