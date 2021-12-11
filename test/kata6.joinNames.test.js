7c3e831ae2c68a7edfbe1a781fdc7032bd119c9e
Wxmnath <Wxmnath@aol.com>
2021-12-11 15:49:21 +0000

    joinNames complete

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,19 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart",
+        },
+        {
+          name: "Lisa",
+        },
+        {
+          name: "Maggie",
+        },
+      ])
+    ).toBe("Bart, Lisa & Maggie");
+  });
 });
