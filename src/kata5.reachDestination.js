443744b98b0ec4036c01896bc9580f5a5c014e29
Wxmnath <Wxmnath@aol.com>
2021-12-11 13:58:28 +0000

    reachDestination complete

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,7 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+  const time = distance / speed;
+  const travelTime = Math.round(time * 2) / 2;
 
+  return `I should be there in ${travelTime} hours.`;
+};
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,7 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+  });
 });
