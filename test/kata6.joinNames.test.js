f5941745b7f8d27c241e460c5fbfd36506dce65e
Wxmnath <Wxmnath@aol.com>
2022-01-09 11:34:29 +0000

    More tests added to joinNames

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -3,17 +3,20 @@ const { joinNames } = require("../src");
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
     expect(
-      joinNames([
-        {
-          name: "Bart",
-        },
-        {
-          name: "Lisa",
-        },
-        {
-          name: "Maggie",
-        },
-      ])
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
     ).toBe("Bart, Lisa & Maggie");
+
+    expect(joinNames([{ name: "Nathan" }, { name: "Joe" }])).toBe(
+      "Nathan & Joe"
+    );
+
+    expect(
+      joinNames([
+        { name: "Bart" },
+        { name: "Lisa" },
+        { name: "Maggie" },
+        { name: "John" },
+      ])
+    ).toBe("Bart, Lisa, Maggie & John");
   });
 });
