10a830b48d5b302041853fb15808dce3fb0ad215
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-04 18:27:53 +0100

    fizzBuzz code and tests

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = number => {};
-
+const fizzBuzz = number => {
+    const isDivisibleBy3 = (number % 3) === 0;
+    const isDivisibleBy5 = (number % 5) === 0;
+    if (isDivisibleBy3 && isDivisibleBy5){
+        return 'FizzBuzz'
+    }
+    else if (isDivisibleBy5) {
+        return 'Buzz';
+    }
+    else if (isDivisibleBy3){
+        return 'Fizz';
+    }
+    else {
+        return number}
+};
+    
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,22 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toBe('Fizz')
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe('Buzz')
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz')
+    expect(fizzBuzz(30)).toBe('FizzBuzz')
+    expect(fizzBuzz(90)).toBe('FizzBuzz')
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe(4)
+    expect(fizzBuzz(11)).toBe(11)
+  });
 });
