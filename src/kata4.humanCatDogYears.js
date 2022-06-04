bd6a6eaca76f84483bb1e37e3d3baaf09bf09aa5
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:20:31 +0100

    Write module to pass humanCatDogYears spec

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,20 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = years => {
+  let dogYears = 0;
+  let catYears = 0;
+
+  if (years === 1) {
+    dogYears = 15;
+    catYears = 15;
+  }
+  if (years === 2) {
+    dogYears = 24;
+    catYears = 24;
+  }
+  if (years > 2) {
+    dogYears = (years - 2) * 5 + 24;
+    catYears = (years - 2) * 4 + 24;
+  }
+  return { dogYears, catYears };
+};
 
 module.exports = humanCatDogYears;
