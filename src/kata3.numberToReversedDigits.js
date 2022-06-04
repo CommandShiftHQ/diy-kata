a3e7df6d54f23ce074e5c05679901dc8a06f72a3
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:17:43 +0100

    Write module to pass numberToReversedDigits spec

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,6 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number =>
+  [...`${number}`]
+    .map(numberAsString => parseInt(numberAsString, 10))
+    .reverse();
 
 module.exports = numberToReversedDigits;
