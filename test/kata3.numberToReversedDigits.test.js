c2b38090b9ad9c4eac88c35e951b1c7ab39d09c9
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:16:50 +0100

    Write spec for numberToReversedDigit

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,8 @@
-const { numberToReversedDigits } = require("../src");
+const numberToReversedDigits = require("../src/kata3.numberToReversedDigits");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    const number = 12345;
+    expect(numberToReversedDigits(number)).toStrictEqual([5, 4, 3, 2, 1]);
+  });
 });
