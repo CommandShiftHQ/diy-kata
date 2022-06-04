5e9f092e96ac94e5f330f77a72261b4d39b0d157
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:10:54 +0100

    Write module to pass the fizzBuzz spec

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,23 @@
-const fizzBuzz = number => {};
+function isMultipleOf3(number) {
+  return number % 3 === 0;
+}
+
+function isMultipleOf5(number) {
+  return number % 5 === 0;
+}
+
+const fizzBuzz = number => {
+  let result = "";
+  if (isMultipleOf3(number)) {
+    result += "Fizz";
+  }
+  if (isMultipleOf5(number)) {
+    result += "Buzz";
+  }
+  if (!(isMultipleOf3(number) || isMultipleOf5(number))) {
+    return number;
+  }
+  return result;
+};
 
 module.exports = fizzBuzz;
