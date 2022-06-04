4b03f7ba8b0a128fee0351e34058e8be5e9215be
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-04 21:26:42 +0100

    kata 5 calculate time to arrive but no test

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,12 @@
-const reachDestination = (distance, speed) => {};
-return 'I should be there in ' + 4.5 + ' hours.'
+
+const reachDestination = (distance, speed) => {
+// console.log('I should be there in ' + 4.5 + ' hours.')
+let time = (distance/speed).toFixed(1);
+const timeWholeHours = Math.trunc(time);
+const decimal = time - timeWholeHours;
+if(decimal <= 0.5){time = timeWholeHours + 0.5}
+else {time = timeWholeHours + 1};
+console.log('I should be there in ' + time + ' hours.')
+};
+reachDestination(44, 10);
 module.exports = reachDestination;
