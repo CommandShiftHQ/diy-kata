aa29440f945e3e68ff95d5a69fddcee303d13e63
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:20:01 +0100

    Write spec for humanCatDogYears

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,22 @@
-const { humanCatDogYears } = require("../src");
+const humanCatDogYears = require("../src/kata4.humanCatDogYears")
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  it("should take an amount of time in human years and return an object containing the number of years in dog years and cat years", () => {
+    expect(humanCatDogYears(1)).toStrictEqual({
+      dogYears: 15,
+      catYears: 15
+    });
+    expect(humanCatDogYears(2)).toStrictEqual({
+      dogYears: 24,
+      catYears: 24
+    });
+    expect(humanCatDogYears(3)).toStrictEqual({
+      dogYears: 29,
+      catYears: 28
+    });
+    expect(humanCatDogYears(4)).toStrictEqual({
+      dogYears: 34,
+      catYears: 32
+    });
+  });
+});
