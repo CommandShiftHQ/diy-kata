dbb88c2e1dc9e03732376202a1228cb6df0d77d5
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-04 21:50:10 +0100

    kata 6 join names together not tested yet

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    let string = '';
+    for (let i = 0; i<namesObj.length - 1; i++)
+    {
+        if(i < (namesObj.length - 2))
+        {string = string + namesObj[i].name + ', '}
+        else if (i < (namesObj.length - 1))
+        {string = string + namesObj[i].name + ' & ' + namesObj[i + 1].name}
+    }   
+    console.log(string);
+}
+let listOfNames = [{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}, {name: 'Marge'}, {name: 'Homer'}];
+joinNames(listOfNames);
 
 module.exports = joinNames;
