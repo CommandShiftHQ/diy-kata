5df595fb9e5029c62f3951ee2d52f9bdb0c62c68
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-06-04 15:45:42 +0100

    Write spec for reachDestination

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,22 @@
-const { reachDestination } = require("../src");
+const reachDestination = require("../src/kata5.reachDestination");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {
+    const [distanceA, speedA] = [44, 10];
+    expect(reachDestination(distanceA, speedA)).toBe(
+      "I should be there in 4.5 hours"
+    );
+    const [distanceB, speedB] = [96, 5];
+    expect(reachDestination(distanceB, speedB)).toBe(
+      "I should be there in 19 hours"
+    );
+    const [distanceC, speedC] = [172, 48];
+    expect(reachDestination(distanceC, speedC)).toBe(
+      "I should be there in 3.5 hours"
+    );
+    const [distanceD, speedD] = [12, 50];
+    expect(reachDestination(distanceD, speedD)).toBe(
+      "I should be there in 0 hours"
+    );
+  });
 });
