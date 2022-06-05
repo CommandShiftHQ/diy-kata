2116cba5eb0ae1ba679656e69dc129d32a9e5e78
Lisa Heffernan <lisaheffernan@outlook.com>
2022-06-05 09:42:29 +0100

    kata 7 code, test not working yet

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,25 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+for (let i = 0; i<employees.length; i++)
+{
+    if(employees[i].name === employeeName)
+    {console.log( employees[i].role)}
+}
+};
+
+const employees = [{
+    name: 'Satti',
+    role: 'Developer'
+  }, 
+  {
+    name: 'Jenny',
+    role: 'Sales Associate'
+  }, 
+  {
+    name: 'Javid',
+    role: 'Human Recommended Reading Assistant'
+  }];
+
+  getEmployerRole('Javid',employees);
+
 
 module.exports = getEmployerRole;
